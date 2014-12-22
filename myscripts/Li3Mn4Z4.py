e270d821620c43e54932da1825fb6a459b83d50b
ldamewood <damewood@physics.ucdavis.edu>
2014-12-22 08:19:18 -0800

    Add Heusler
    Add Heusler structure
    Fix Li3 script

--- myscripts/Li3Mn4Z4.py
+++ myscripts/Li3Mn4Z4.py
@@ -49,20 +49,21 @@ pseudos = all_pseudos()
 flows = []
 for (Z, i) in itertools.product(['P','N','Si'],range(4)):
     x = 4 - i
-    name = 'Li%dMn4%s4' % (x,Z)
+    name = u'Li%d Mn4 %s4' % (x,Z)
     print(name)
     flow = Flow(manager = manager, workdir = os.path.join(workdir, name))
     for (phase) in ["alpha","beta","gamma"]:
         structure = HalfHeusler(['Li','Mn',Z], phase, acell_opt[Z][phase])
         structure.make_supercell([[-1,1,1],[1,-1,1],[1,1,-1]])
-        structure.remove_sites(range(x))
-        structure.sort(key=lambda x: x.specie.Z)
+        structure.remove_sites(list(range(i)))
+        structure.sort(key=lambda j: j.specie.Z)
+        assert name == structure.formula
         spins = numpy.zeros([len(structure),3])
-        for i,atom in enumerate(structure):
+        for j,atom in enumerate(structure):
             if atom.specie.symbol == 'Li':
-                spins[i,2] = 1.
+                spins[j,2] = 1.
             if atom.specie.symbol == 'Mn':
-                spins[i,2] = 3.
+                spins[j,2] = 3.
         ion_input = RelaxStrategy(AbiStructure(structure), pseudos, ksampling,
             relax_ion, accuracy="high", smearing = "fermi_dirac:0.025 eV",
             ecut = 40., pawecutdg = 80., chkprim = 0, tolmxf = 5.e-6,
@@ -82,4 +83,4 @@ def rapidfire():
     for flow in flows:
         flow.rapidfire()
 
-build_and_pickle_dump()
+#build_and_pickle_dump()
--- myscripts/structure.py
+++ myscripts/structure.py
@@ -82,6 +82,26 @@ def ZincBlende(elements, a):
     lattice = Lattice(float(a) * numpy.array(fcc_lattice))
     return Structure(lattice, elements, positions).get_sorted_structure()
 
+def Heusler(elements, phase, a):
+    positions = {
+        'alpha' : [ wyckoff(216,'4c'),
+                    wyckoff(216,'4b'),
+                    wyckoff(216,'4a'),
+                    wyckoff(216,'4d')],
+        'beta'  : [ wyckoff(216,'4b'),
+                    wyckoff(216,'4a'),
+                    wyckoff(216,'4c'),
+                    wyckoff(216,'4d')],
+        'gamma' : [ wyckoff(216,'4a'),
+                    wyckoff(216,'4c'),
+                    wyckoff(216,'4b'),
+                    wyckoff(216,'4d')],
+    }
+    if hasattr(a,'to') and callable(getattr(a,'to')):
+        a = a.to('bohr')
+    lattice = Lattice(float(a) * numpy.array(fcc_lattice))
+    return Structure(lattice, elements, positions[phase]).get_sorted_structure()
+
 def HalfHeusler(elements, phase, a):
     positions = {
         'alpha' : [ wyckoff(216,'4c'),
