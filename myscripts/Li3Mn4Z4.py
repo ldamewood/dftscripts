3dcaa5bc34c4a405f3ef006af6516d3503f955f5
ldamewood <damewood@physics.ucdavis.edu>
2015-01-01 11:14:17 -0800

    New cell values
    cell values
    longer nstep
    restart available

--- myscripts/Li3Mn4Z4.py
+++ myscripts/Li3Mn4Z4.py
@@ -25,19 +25,55 @@ manager = TaskManager.from_user_config()
 
 acell_opt = {
     'N': {
-            'alpha': 4.961,
-            'beta' : 4.912,
-            'gamma': 5.139,
+            'Li1' : {
+                'alpha': 4.961,
+                'beta' : 4.404,
+                'gamma': 4.996,
+            },
+            'Li2' : {
+                'alpha': 4.500,
+                'beta' : 4.500,
+                'gamma': 4.500,
+            },
+            'Li3' : {
+                'alpha': 4.552,
+                'beta' : 4.552,
+                'gamma': 4.552,
+            },
          },
     'P': {
-            'alpha': 5.600,
-            'beta' : 5.717,
-            'gamma': 5.715,
+            'Li1' : {
+                'alpha': 5.300,
+                'beta' : 5.422,
+                'gamma': 5.260,
+            },
+            'Li2' : {
+                'alpha': 5.220,
+                'beta' : 5.220,
+                'gamma': 5.220,
+            },
+            'Li3' : {
+                'alpha': 5.502,
+                'beta' : 5.502,
+                'gamma': 5.502,
+            },
          },
     'Si': {
-            'alpha': 5.629,
-            'beta' : 5.778,
-            'gamma': 5.788,
+            'Li1' : {
+                'alpha': 4.894,
+                'beta' : 5.527,
+                'gamma': 5.517,
+            },
+            'Li2' : {
+                'alpha': 4.894,
+                'beta' : 5.527,
+                'gamma': 5.517,
+            },
+            'Li3' : {
+                'alpha': 5.629,
+                'beta' : 5.778,
+                'gamma': 5.788,
+            },
          },
 }
 
@@ -47,13 +83,14 @@ relax_ioncell = RelaxationMethod(ionmov = 2, optcell = 1)
 
 pseudos = all_pseudos()
 flows = []
-for (Z, i) in itertools.product(['P','N','Si'],range(4)):
+for (Z, i) in itertools.product(['P','N','Si'],range(1,4)):
     x = 4 - i
     name = u'Li%dMn4%s4' % (x,Z)
     print(name)
     flow = Flow(manager = manager, workdir = os.path.join(workdir, name))
+    li = u'Li%d' % x
     for (phase) in ["alpha","beta","gamma"]:
-        structure = HalfHeusler(['Li','Mn',Z], phase, acell_opt[Z][phase])
+        structure = HalfHeusler(['Li','Mn',Z], phase, acell_opt[Z][li][phase])
         structure.make_supercell([[-1,1,1],[1,-1,1],[1,1,-1]])
         structure.remove_sites(list(range(i)))
         structure.sort(key=lambda j: j.specie.Z)
@@ -67,7 +104,7 @@ for (Z, i) in itertools.product(['P','N','Si'],range(4)):
         ion_input = RelaxStrategy(AbiStructure(structure), pseudos, ksampling,
             relax_ion, accuracy="high", smearing = "fermi_dirac:0.025 eV",
             ecut = 40., pawecutdg = 80., chkprim = 0, tolmxf = 5.e-6,
-            spinat = spins)
+            spinat = spins, restartxf = -2, nband = 60, nstep = 100)
         ioncell_input = ion_input.copy()
         ioncell_input.relax_algo = relax_ioncell
         work = RelaxWork(ion_input, ioncell_input, manager = manager)
@@ -82,5 +119,14 @@ def build_and_pickle_dump():
 def rapidfire():
     for flow in flows:
         flow.rapidfire()
+
+def get_status():
+    for flow in flows:
+        for work in flow:
+            for task in work:
+                try:
+                    task.check_status()
+                except TypeError:
+                    pass
 
 #build_and_pickle_dump()
