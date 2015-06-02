a7f5eed1331637d5dbae19d50db893e7dcc9fd8b
ldamewood <damewood@physics.ucdavis.edu>
2015-06-01 17:26:02 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ from setuptools import setup, find_packages
 SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
 
 setup_args = dict(
-    name             = 'dftscripts',
-    version          = '0.1',
-    description      = 'Scripts on top of pymatgen',
-    author           = 'Liam Damewood',
-    author_email     = 'damewood@physics.ucdavis.edu',
-    package_dir      = {'dftscripts' : 'dftscripts'},
-    packages         = ['dftscripts'],
-    scripts          = glob.glob(os.path.join(SETUP_PTH, "scripts", "*"))
+    name='dftscripts',
+    version='0.1',
+    description='Scripts on top of pymatgen',
+    author='Liam Damewood',
+    author_email='damewood@physics.ucdavis.edu',
+    package_dir={'dftscripts': 'dftscripts'},
+    packages=find_packages(),
+    scripts=glob.glob(os.path.join(SETUP_PTH, "scripts", "*"))
 )
 
 if __name__ == '__main__':
